529052435f8c790a097a2613c1cc624a5d3aa0f4
JuanGarzon616 <53374654+JuanGarzon616@users.noreply.github.com>
2021-05-24 16:49:23 -0500

    treyrtyerty

---
+++ src/components/UserComponents/header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex justify-end flex-wrap content-center h-16 bg-gradient-to-r from-blue-200 via-blue-200 to-blue-50 pr-9 ">
+    <img class="h-5/6 rounded-full border-4 border-blue-200" v-bind:src=image alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "user-header",
+  data(){
+    return{
+      image: JSON.parse(localStorage.getItem('user')).image
+    }
+  },
+  methods:{
+    hola(){
+      console.log(JSON.parse(localStorage.getItem('user')).image)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/layouts/users/ulogin.vue
+++ src/views/layouts/users/ulogin.vue
@@ -1,12 +1,20 @@
 <template>
-  <h1>user login asdasd</h1>
+  <div>
+    <userHeader></userHeader>
+    <h1>user login asdasd</h1>
+  </div>
+
 </template>
 
 <script>
+import userHeader from "@/components/UserComponents/header";
 
-
-
-
+export default {
+  name: 'ulogin',
+  components: {
+    userHeader
+  }
+}
 </script>
 
 <style scoped>
