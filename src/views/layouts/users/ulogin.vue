3b50cd0b050e0851d6e7e17736ff51acd9ff2a81
JuanGarzon616 <53374654+JuanGarzon616@users.noreply.github.com>
2021-06-03 18:00:06 -0500

    yeryerty

--- src/views/layouts/users/ulogin.vue
+++ src/views/layouts/users/ulogin.vue
@@ -1,5 +1,5 @@
 <template>
-<!--    <div class="h-16 max-w-screen"></div>-->
+<!-- md:z-10 max-h-52  block border-collapse border border-green-800 overflow-x-scroll   <div class="h-16 max-w-screen"></div>-->
     <div class="max-w-screen flex md:flex-row-reverse flex-col ">
 
       <div class="w-full">
@@ -7,28 +7,34 @@
           <userHeader class="z-20"></userHeader>
         </span>
         <div class="pl-10 pt-36 md:pl-24 min-h-screen bg-gray-200 ">
-          <h1 class="text-3xl z-0 md:z-10"><b>Bienvenido</b></h1>
-          <p class="text-xl">Aqui puedes ver y gestionar tus Pqr.</p>
-          <table class="max-h-50 w-52 block border-collapse border border-green-800 overflow-x-scroll">
-            <tr>
-              <td>Asunto</td>
-              <td>Fecha de Creacion</td>
-              <td>Estado</td>
-              <td>Ver mas</td>
-            </tr>
-            <tr v-for="(pqr, index) in pqrsUser" :key="index">
-              <td>{{pqr.affair}}</td>
-              <td>{{pqr.date}}</td>
-              <td>{{pqr.fk_status_id}}</td>
-              <td>{{pqr.affair}}</td>
-            </tr>
-          </table>
+          <h1 class="text-3xl z-10"><b>Bienvenido</b></h1>
+<!--          <p class="text-xl">Aqui puedes ver y gestionar tus Pqr.</p>-->
+<!--          <table class="table-fixed w-1/2 block  overflow-x-scroll">-->
+<!--            <thead>-->
+<!--              <tr class="max-h-52 ">-->
+<!--                <th class="w-1/3 ">Asunto</th>-->
+<!--                <th class="w-1/3 ">Descripcion</th>-->
+<!--                <th class="w-1/3 ">Fecha de Creacion</th>-->
+<!--                <th class="w-1/3 ">Estado</th>-->
+<!--                <th class="w-1/3 ">Ver mas</th>-->
+<!--              </tr>-->
+<!--            </thead>-->
+<!--            <tbody>-->
+<!--              <tr v-for="(pqr, index) in pqrsUser" :key="index">-->
+<!--                <td class="border-collapse border border-green-800">{{pqr.affair}}</td>-->
+<!--                <td class="border-collapse border border-green-800">{{pqr.description}}</td>-->
+<!--                <td class="border-collapse border border-green-800">{{pqr.date}}</td>-->
+<!--                <td class="border-collapse border border-green-800">{{pqr.fk_status_id}}</td>-->
+<!--                <td class="border-collapse border border-green-800">{{pqr.affair}}</td>-->
+<!--              </tr>-->
+<!--            </tbody>-->
+<!--          </table>-->
         </div>
       </div>
       <span>
         <p class="md:w-60 w-0 md:min-h-screen h-0" v-if="this.$store.getters.bar"></p>
       </span>
-      <span class="fixed inset-x-0 top-16">
+      <span class="fixed inset-x-0 top-16 z-0">
         <task v-if="this.$store.getters.bar"></task>
       </span>
     </div>
