70d39dfd6bea64f353b265fe104d811efec9f69c
JuanGarzon616 <53374654+JuanGarzon616@users.noreply.github.com>
2021-06-04 19:24:26 -0500

    funciona el live search de momento

--- src/store/modules/PqrsModules.js
+++ src/store/modules/PqrsModules.js
@@ -6,6 +6,7 @@ export default {
     namespaced: true,
     state: {
         PqrUrl: 'http://127.0.0.1:8000/api/pqr/user',
+        PqrUrlAffair: 'http://127.0.0.1:8000/api/pqr/affair/',
         pqrs: [],
         responses: [],
         attchments: [],
@@ -23,14 +24,27 @@ export default {
                 console.log(response)
                 if (response.data.status === 'Token is Expired') {
                     localStorage.clear()
-                    Swal.fire('','Su sesion ha cadudaco.','info')
+                    Swal.fire('', 'Su sesion ha cadudaco.', 'info')
                     router.push('/')
                 }
                 commit('setPqrs', response.data)
             }).catch(error => {
                 console.log(error)
             })
-        }
+        },
+        getPqrUserAffair({state,commit}, valor) {
+            console.log(valor)
+            axios.post(`${state.PqrUrlAffair}`, valor, {
+                'headers': {
+                    'Authorization': `Bearer ${JSON.parse(localStorage.getItem("token"))}`,
+                }
+            }).then(response => {
+                commit('setPqrs',response.data)
+                console.log(response)
+            }).catch((error => {
+                console.log(error)
+            }))
+        },
     },
     getters: {
         pqrsUser: (state) => state.pqrs,
--- src/views/layouts/users/ulogin.vue
+++ src/views/layouts/users/ulogin.vue
@@ -8,12 +8,13 @@
       <div class="pl-0 pt-36 md:pl-24 min-h-screen bg-gray-200 flex flex-col">
         <h1 class="text-3xl z-0 lg:z-10"><b>Bienvenido</b></h1>
         <p class="text-xl z-0 lg:z-10">Aqui puedes ver y gestionar tus Pqr.</p>
-        <input v-on:change="event=>submit(event)" type="text"
+        <input @keyup="event=>submit(event)" type="text"
                class="mt-10 ml-0 md:ml-10 focus:outline-none focus:ring focus:border-blue-300 z-0 lg:z-10 rounded-full w-full md:w-96 p-3"
-               placeholder="Buscar por asunto">
+               placeholder="Buscar por asunto" v-model="valor">
+        <p>{{ valor }}</p>
         <div class="flex pl-0 md:pl-10 ">
-          <table class="block overflow-scroll w-full md:w-11/12 z-0 lg:z-10 bg-white rounded-lg  sm:shadow-lg my-5"
-                 style="height: 60vh">
+          <table
+              class="block overflow-scroll w-full md:w-11/12 z-0 lg:z-10 bg-white rounded-lg sm:shadow-lg my-5 max-h-96">
             <thead class=" w-full">
             <tr class="bg-teal-400  rounded-l-lg sm:rounded-none">
               <th class="p-3 text-left w-3">#</th>
@@ -26,14 +27,18 @@
             </thead>
             <tbody class="">
             <tr v-for="(pqr, index) in pqrsUser" :key="index" class="">
-              <td class="border-grey-light border hover:bg-gray-100 p-3 ">{{ index }}</td>
-              <td class="border-grey-light border hover:bg-gray-100 p-3 ">{{ pqr.affair }}</td>
-              <td class="border-grey-light border hover:bg-gray-100 p-3 ">{{ pqr.description }}</td>
-              <td class="border-grey-light border hover:bg-gray-100 p-3 ">{{ pqr.date }}</td>
-              <td v-if="pqr.fk_status_id===1" class="border-grey-light border hover:bg-gray-100 p-3 ">Activo</td>
-              <td v-if="pqr.fk_status_id===2" class="border-grey-light border hover:bg-gray-100 p-3 ">Resuelto</td>
-              <td v-if="pqr.fk_status_id===3" class="border-grey-light border hover:bg-gray-100 p-3 ">En proceso</td>
-              <td class="border-grey-light border hover:bg-gray-100 p-3 ">ver mas</td>
+
+            <td class="border-grey-light border hover:bg-gray-100 p-3 ">{{ index }}</td>
+            <td class="border-grey-light border hover:bg-gray-100 p-3 ">{{ pqr.affair }}</td>
+            <td class="border-grey-light border hover:bg-gray-100 p-3 ">{{ pqr.description }}</td>
+            <td class="border-grey-light border hover:bg-gray-100 p-3 ">{{ pqr.date }}</td>
+            <td v-if="pqr.fk_status_id===1" class="border-grey-light border hover:bg-gray-100 p-3 ">Activo</td>
+            <td v-if="pqr.fk_status_id===2" class="border-grey-light border hover:bg-gray-100 p-3 ">Resuelto</td>
+            <td v-if="pqr.fk_status_id===3" class="border-grey-light border hover:bg-gray-100 p-3 ">En proceso</td>
+            <td class="border-grey-light border hover:bg-gray-100 p-3 ">ver mas</td>
+            </tr>
+            <tr v-if="pqrsUser.affair">
+              <td colspan="6" class="p-10 text-center">No se encontraron resultados.</td>
             </tr>
             </tbody>
           </table>
@@ -57,19 +62,31 @@ import {mapActions, mapGetters} from "vuex";
 
 export default {
   name: 'ulogin',
+  data() {
+    return {
+      valor: ''
+    }
+  },
   components: {
     userHeader,
     task
   },
   created() {
     this.getPqrUser()
+    // this.getPqrUserAffair(this.valor)
   },
   computed: {
     ...mapGetters('d', ['pqrsUser'])
   },
   methods: {
     ...mapActions('d', ['getPqrUser']),
+    ...mapActions('d', ['getPqrUserAffair']),
     submit(event) {
+      let value = {
+        id: JSON.parse(localStorage.getItem('user')).id,
+        name: event.target.value
+      }
+      this.getPqrUserAffair(value)
       console.log(event.target.value)
     }
   }
@@ -85,6 +102,8 @@ body {
 @media (min-width: 640px) {
   table {
     display: inline-table !important;
+    /*max-height: 60vh;*/
+    /*min-height: 60px;*/
   }
 
   thead tr:not(:first-child) {
