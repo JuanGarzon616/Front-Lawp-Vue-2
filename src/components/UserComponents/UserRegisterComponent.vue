9b5364416e25272a72ed59cd2e723fb1bdc5d723
JuanGarzon616 <53374654+JuanGarzon616@users.noreply.github.com>
2021-06-13 14:38:45 -0500

    ewrtwert

--- src/components/UserComponents/UserRegisterComponent.vue
+++ src/components/UserComponents/UserRegisterComponent.vue
@@ -1,21 +1,23 @@
 <template>
   <div class="flex justify-center flex-wrap content-center bg-gray-100 min-h-screen">
 
-    <form v-on:submit.prevent="submit"  class="bg-white w-full sm:w-1/2 h-auto rounded-lg shadow-2xl">
+    <form v-on:submit.prevent="submit" class="bg-white w-full sm:w-1/2 h-auto rounded-lg shadow-2xl">
       <div class="bg-gray-700 text-gray-200 rounded-lg p-5">
-        <h1 class="text-3xl" >registro usuario <i class="fas fa-desktop"></i></h1>
-        <h1 class="py-5" >Crea una cuenta gratis para poder usar nuestros servicios.</h1>
+        <h1 class="text-3xl">registro usuario <i class="fas fa-desktop"></i></h1>
+        <h1 class="py-5">Crea una cuenta gratis para poder usar nuestros servicios.</h1>
       </div>
       <div class="grid grid-cols-1 md:grid-cols-2 p-10 gap-4">
         <div class="divin" :class="{'text-red-600': $v.document.$error}">
           <label for="document">Documento de identidad.<i class="fas fa-address-card"></i></label>
           <p v-if="!$v.document.minLength">Documento muy corto</p>
           <p v-if="!$v.document.maxLength">Documento exede lo permitido</p>
-          <input class="inputs" :class="{ 'postin': $v.document.$error }" v-model.trim="$v.document.$model" id="document" type="number">
+          <input class="inputs" :class="{ 'postin': $v.document.$error }" v-model.trim="$v.document.$model"
+                 id="document" type="number">
         </div>
         <div class="divin" :class="{'text-red-600': $v.fk_document_type_id.$error}">
           <label for="typedocument">Tipo De Documento.</label>
-          <select class="inputs" :class="{ 'postin': $v.fk_document_type_id.$error }"  v-model.trim="$v.fk_document_type_id.$model" name="" id="typedocument">
+          <select class="inputs" :class="{ 'postin': $v.fk_document_type_id.$error }"
+                  v-model.trim="$v.fk_document_type_id.$model" name="" id="typedocument">
             <option disabled value="">Seleccione un elemento</option>
             <option value="1">Cedula de Ciudadania</option>
             <option value="2">Targeta de Identidad</option>
@@ -27,61 +29,72 @@
           <label for="names">Nombres.</label>
           <p v-if="!$v.names.minLength">Nombre muy corto</p>
           <p v-if="!$v.names.maxLength">Nombre exede lo permitido</p>
-          <input class="inputs" :class="{ 'postin': $v.names.$error }"  v-model.trim="$v.names.$model" id="names" type="text">
+          <input class="inputs" :class="{ 'postin': $v.names.$error }" v-model.trim="$v.names.$model" id="names"
+                 type="text">
         </div>
         <div class="divin" :class="{'text-red-600': $v.last_names.$error}">
           <label for="lnames">Apellidos.</label>
           <p v-if="!$v.last_names.minLength">Apellido muy corto</p>
           <p v-if="!$v.last_names.maxLength">Apellido exede lo permitido</p>
-          <input class="inputs" :class="{ 'postin': $v.last_names.$error }" v-model.trim="$v.last_names.$model" id="lnames" type="text">
+          <input class="inputs" :class="{ 'postin': $v.last_names.$error }" v-model.trim="$v.last_names.$model"
+                 id="lnames" type="text">
         </div>
         <div class="divin" :class="{'text-red-600': $v.direcction.$error}">
           <label for="dir">Direccion.</label>
           <p v-if="!$v.direcction.maxLength">Direccion exede lo permitido</p>
-          <input class="inputs" :class="{ 'postin': $v.direcction.$error }" v-model.trim="$v.direcction.$model" id="dir" type="text">
+          <input class="inputs" :class="{ 'postin': $v.direcction.$error }" v-model.trim="$v.direcction.$model" id="dir"
+                 type="text">
         </div>
         <div class="divin" :class="{'text-red-600': $v.tellephone1.$error}">
           <label for="tell1">Telefono 1.</label>
           <p v-if="!$v.tellephone1.minLength">Telefono muy corto</p>
           <p v-if="!$v.tellephone1.maxLength">Telefono exede lo permitido</p>
-          <input class="inputs" :class="{ 'postin': $v.tellephone1.$error }" v-model.trim="$v.tellephone1.$model" id="tell1" type="number">
+          <input class="inputs" :class="{ 'postin': $v.tellephone1.$error }" v-model.trim="$v.tellephone1.$model"
+                 id="tell1" type="number">
         </div>
         <div class="divin" :class="{'text-red-600': $v.tellephone2.$error}">
           <label for="tell2">Telefono 2.</label>
-          <p  v-if="!$v.tellephone2.minLength">Telefono muy corto</p>
+          <p v-if="!$v.tellephone2.minLength">Telefono muy corto</p>
           <p v-if="!$v.tellephone2.maxLength">Telefono exede lo permitido</p>
-          <input class="inputs" :class="{ 'postin': $v.tellephone2.$error }" v-model.trim="$v.tellephone2.$model" id="tell2" type="number">
+          <input class="inputs" :class="{ 'postin': $v.tellephone2.$error }" v-model.trim="$v.tellephone2.$model"
+                 id="tell2" type="number">
         </div>
         <div class="divin" :class="{'text-red-600': $v.mail.$error}">
           <label for="mail">Correo.</label>
           <p v-if="!$v.mail.maxLength">Correo exede lo permitido</p>
-          <p v-if="!$v.mail.email" >Ingresa un correo valido</p>
-          <input class="inputs" :class="{ 'postin': $v.mail.$error }" v-model.trim="$v.mail.$model" id="mail" type="email">
+          <p v-if="!$v.mail.email">Ingresa un correo valido</p>
+          <input class="inputs" :class="{ 'postin': $v.mail.$error }" v-model.trim="$v.mail.$model" id="mail"
+                 type="email">
         </div>
         <div class="divin" :class="{'text-red-600': $v.password2.$error}">
           <label for="contra1">Contraseña</label>
           <p v-if="!$v.password2.minLength">Contraseña muy corto</p>
-          <input class="inputs" :class="{ 'postin': $v.password2.$error }" v-model.trim="$v.password2.$model" id="contra1" type="password">
+          <input class="inputs" :class="{ 'postin': $v.password2.$error }" v-model.trim="$v.password2.$model"
+                 id="contra1" type="password">
         </div>
         <div class="divin" :class="{'text-red-600': $v.password.$error}">
           <label for="contra2">Confirmar Contraseña</label>
-          <p v-if="!$v.password.sameAs" >La contraseña debe ser igual.</p>
-          <input class="inputs" :class="{ 'postin': $v.password.$error }" v-model.trim="$v.password.$model" id="contra2" type="password">
+          <p v-if="!$v.password.sameAs">La contraseña debe ser igual.</p>
+          <input class="inputs" :class="{ 'postin': $v.password.$error }" v-model.trim="$v.password.$model" id="contra2"
+                 type="password">
         </div>
         <div class="divin" :class="{'text-red-600': $v.depar.$error}">
           <label for="departament">Departamento</label>
-          <select class="inputs" :class="{ 'postin': $v.depar.$error }" v-model.trim="$v.depar.$model" v-on:click="muni" id="departament">
+          <select class="inputs" :class="{ 'postin': $v.depar.$error }" v-model.trim="$v.depar.$model" v-on:click="muni"
+                  id="departament">
             <option disabled value="">Seleccione un elemento</option>
-            <option v-for="(item, index) in allDepartaments" :key="index" v-bind:value="{depar: item.id}" >
+            <option v-for="(item, index) in allDepartaments" :key="index" v-bind:value="{depar: item.id}">
               {{ item.name }}
             </option>
           </select>
         </div>
         <div class="divin" :class="{'text-red-600': $v.fk_municipality_id.$error}">
           <label for="muni">Municipio</label>
-          <select class="inputs" :class="{ 'postin': $v.fk_municipality_id.$error }" v-model.trim="$v.fk_municipality_id.$model" name="muni" id="muni">
+          <select class="inputs" :class="{ 'postin': $v.fk_municipality_id.$error }"
+                  v-model.trim="$v.fk_municipality_id.$model" name="muni" id="muni">
             <option disabled value="">Seleccione un elemento</option>
-            <option v-for="(item, index) in allMunicipalities" :key="index" v-bind:value="{fk_municipality_id: item.id}">
+            <option v-for="(item, index) in allMunicipalities" :key="index"
+                    v-bind:value="{fk_municipality_id: item.id}">
               {{ item.name }}
             </option>
           </select>
@@ -90,7 +103,7 @@
         <div class="grid col-span-1 md:col-span-2" :class="{ 'postin': $v.buss.$error }">
           <p>¿Es cuenta empresarial?</p>
           <label for="si">Si <input id="si" name="buss" type="radio" v-model="buss" value="si"></label>
-          <label for="no">No <input  id="no" name="buss" type="radio" v-model="buss" value="no"></label>
+          <label for="no">No <input id="no" name="buss" type="radio" v-model="buss" value="no"></label>
         </div>
         <input type="submit" class="text-gray-200 bg-gray-700 hover:bg-gray-500 py-2 px-4 rounded w-20" value="Crear">
       </div>
@@ -100,17 +113,14 @@
 </template>
 <script>
 
-import { mapGetters, mapActions } from 'vuex'
-import { required, minLength, maxLength, sameAs, email } from 'vuelidate/lib/validators'
-//import router from "@/router"
+import {mapGetters, mapActions} from 'vuex'
+import {required, minLength, maxLength, sameAs, email} from 'vuelidate/lib/validators'
 import Swal from "sweetalert2"
 
-export default{
+export default {
   name: 'UserCreate',
-  components: {
-
-  },
-  data(){
+  components: {},
+  data() {
     return {
       document: '',
       fk_document_type_id: '',
@@ -145,17 +155,17 @@ export default{
     },
     names: {
       required,
-      minLength: minLength (3),
-      maxLength: maxLength (50)
+      minLength: minLength(3),
+      maxLength: maxLength(50)
     },
     last_names: {
       required,
-      minLength: minLength (3),
-      maxLength: maxLength (50)
+      minLength: minLength(3),
+      maxLength: maxLength(50)
     },
     direcction: {
       required,
-      maxLength: maxLength (50)
+      maxLength: maxLength(50)
     },
     tellephone1: {
       required,
@@ -191,17 +201,17 @@ export default{
     this.getDepartaments()
   },
   computed: {
-    ...mapGetters('c',['allDepartaments']),
-    ...mapGetters('c',['allMunicipalities']),
+    ...mapGetters('c', ['allDepartaments']),
+    ...mapGetters('c', ['allMunicipalities']),
   },
   methods: {
-    ...mapActions('c',['getDepartaments']),
-    ...mapActions('c',['getMunicipalities']),
-    ...mapActions('a',['saveUser']),
-    muni(){
+    ...mapActions('c', ['getDepartaments']),
+    ...mapActions('c', ['getMunicipalities']),
+    ...mapActions('a', ['saveUser']),
+    muni() {
       this.getMunicipalities(JSON.parse(JSON.stringify(this.depar)).depar);
     },
-    createUser(){
+    createUser() {
       this.saveUser({
         document: this.document,
         names: this.names,
@@ -220,29 +230,29 @@ export default{
         localStorage.setItem('token', JSON.stringify(response.data.token))
         localStorage.setItem('user', JSON.stringify(response.data.user))
 
-        if(response.status===201){
-          Swal.fire("Usuario creado","usuario creado satisfactoriamente","success")
-        }else {
+        if (response.status === 201) {
+          Swal.fire("Usuario creado", "usuario creado satisfactoriamente", "success")
+        } else {
           console.log("algo paso")
         }
 
-        if(this.buss === 'si'){
+        if (this.buss === 'si') {
           this.$router.push({name: 'BusinessRegister'})
-        }else{
+        } else {
           this.$router.push({name: 'ulogin'})
         }
 
-      }).catch( function (error){
+      }).catch(function (error) {
         console.log(error.response.data.status)
         console.log(error.response)
-        if(error.response.data.messages.mail && error.response.data.messages.document){
-          Swal.fire("Error",'Email y Numero de identificacion ya tomados.','error');
-        }else if(error.response.data.messages.document){
-          Swal.fire("Error",'Numero de identificacion ya tomado.','error');
-        }else if(error.response.data.messages.mail){
-          Swal.fire("Error",'Correo  ya tomado.','error');
-        }else if(error.response.status===422){
-          Swal.fire("Error",'A ocurrido un error revisa los datos.','error');
+        if (error.response.data.messages.mail && error.response.data.messages.document) {
+          Swal.fire("Error", 'Email y Numero de identificacion ya tomados.', 'error');
+        } else if (error.response.data.messages.document) {
+          Swal.fire("Error", 'Numero de identificacion ya tomado.', 'error');
+        } else if (error.response.data.messages.mail) {
+          Swal.fire("Error", 'Correo  ya tomado.', 'error');
+        } else if (error.response.status === 422) {
+          Swal.fire("Error", 'A ocurrido un error revisa los datos.', 'error');
         }
       })
     },
@@ -263,12 +273,11 @@ export default{
   }
 
 
-
 }
 </script>
 
 <style scoped>
-form{
+form {
   font-family: 'Roboto', sans-serif;
 
 }
--- src/views/layouts/users/createpqr.vue
+++ src/views/layouts/users/createpqr.vue
@@ -6,23 +6,74 @@
           <userHeader class="z-20"></userHeader>
         </span>
       <div class="pl-0 pt-36 md:pl-24 min-h-screen bg-gray-200 flex flex-col">
-        <h1 class="text-3xl z-0 lg:z-10"><b>Bienvenido</b></h1>
-        <p class="text-xl z-0 lg:z-10">Aqui puedes ver y gestionar tus Pqr.</p>
-        <div>
-          <form class="flex flex-col">
-            <label for="affair">Asunto</label>
-            <input type="text" class="" id="affair">
-            <label for="">Descripcion</label>
-            <!--            <textarea name="" id="desc" cols="30" rows="10"></textarea>-->
-<!--            <vue-editor  :editorToolbar="customToolbar"></vue-editor>-->
-            <p @paste="sanitize">{{ content }}</p>
-<!--            <p>Caracteres maximos 60.000 usted lleva ({{ content.length }})</p>-->
-            <select class="z-0 lg:z-10" name="" id="">
-              <option disabled value="">Seleccione un elemento</option>
-              <option v-for="(item, index) in this.$store.getters['b/getNamesBusi']" :key="index" value="">
-                {{ item.bussiness_name }}, {{ item.mail }}
-              </option>
-            </select>
+        <div class="pb-10">
+          <h1 class="text-3xl z-0 lg:z-10"><b>Bienvenido</b></h1>
+          <p class="text-xl z-0 lg:z-10">Aqui puedes crear tus Pqr.</p>
+        </div>
+        <!--        -->
+        <div class="z-0 lg:z-10 bg-white p-10 mb-20 w-full lg:w-11/12 rounded-lg">
+          <form class="flex flex-col w-full">
+            <label for="affair" class="text-2xl">Asunto</label>
+            <input type="text"
+                   class=" w-1/3 p-1 border focus:outline-none focus:ring-2 focus:border-transparent rounded-lg shadow-2xl"
+                   id="affair">
+            <div class="py-10 grid gap-10 grid-cols-1 md:grid-cols-3 flex justify-between">
+              <div class="divin">
+                <label for="empre" class="text-2xl">Empresa destinataria</label>
+                <p>Recuerde revisar el correo correcto</p>
+                <select
+                    class=" z-0 lg:z-10 p-1 border focus:outline-none focus:ring-2 focus:border-transparent rounded-lg shadow-2xl"
+                    name="" id="empre">
+                  <option disabled value="">Seleccione un elemento</option>
+                  <option v-for="(item, index) in this.$store.getters['b/getNamesBusi']" :key="index" value="">
+                    {{ item.bussiness_name }}, {{ item.mail }}
+                  </option>
+                </select>
+              </div>
+              <div class="divin">
+                <label for="pqrtype" class="text-2xl">Tipo de PQR</label>
+                <p></p>
+                <select name="" id="pqrtype"
+                        class=" z-0 lg:z-10 p-1 border focus:outline-none focus:ring-2 focus:border-transparent rounded-lg shadow-2xl">
+                  <option disabled value="">Seleccione un elemento</option>
+                  <option value="1">pregunta</option>
+                  <option value="2">queja</option>
+                  <option value="3">reclamo</option>
+                </select>
+              </div>
+              <div class="divin">
+                <label for="d" class="text-2xl">Fecha del momento</label>
+                <p>fechas antes de hoy.</p>
+                <input type="date" id="d" class="inputs">
+              </div>
+            </div>
+            <label for="desc" class="text-2xl">Descripcion</label>
+            <p>Recuerda ser lo mas puntual posible</p>
+            <vue-editor :editorToolbar="customToolbar" v-model="content" id="desc"></vue-editor>
+            <p>Caracteres maximos 60.000 usted lleva ({{ content.length }})</p>
+            <div class="mt-10">
+              <div>
+                <p class="text-2xl">Adjuntos</p>
+                <p>Agrega solo si crees necesario.</p>
+                <div class="text-3xl">
+                  <i v-on:click="sum" class="fas fa-plus"></i>
+                  <i v-on:click="less" class="fas fa-minus"></i>
+                </div>
+
+              </div>
+              <div class="grid gap-3 grid-cols-1 md:grid-cols-2">
+                <div v-for="(id, index) in numadj" :key="index" class="grid gap-3 grid-cols-1">
+                  <label for="url">Url (no necesria)</label>
+                  <input class="inputs" type="text" id="url">
+                  <label for="fil"></label>
+                  <input class="inputs" type="file" id="fil">
+                </div>
+              </div>
+            </div>
+            <div class="mt-10">
+              <input class="text-gray-200 bg-gray-700 hover:bg-gray-500 py-2 px-4 rounded w-20" type="submit" value="enviar">
+            </div>
+
           </form>
         </div>
       </div>
@@ -40,18 +91,21 @@
 import userHeader from "@/components/UserComponents/header";
 import task from "@/components/UserComponents/task";
 import {mapActions} from "vuex";
-// import {VueEditor} from "vue2-editor";
+import {VueEditor} from "vue2-editor";
 
 export default {
   name: "createpqr",
   components: {
     userHeader,
     task,
-    // VueEditor,
+    VueEditor,
   },
   data() {
     return {
-      content: `&#60script&#62${alert("hola")}&#60/script&#62`,
+      content: '',
+      numadj: 0,
+      affair: '',
+
       //content: `&#60script&#62${alert("hola")}&#60/script&#62`,
       customToolbar: [
         [{font: []}],
@@ -64,11 +118,14 @@ export default {
           {align: "right"},
           {align: "justify"}
         ],
-        [{list: "ordered"}, {list: "bullet"}, {list: "check"}],
+        [{list: "ordered"}, {list: "bullet"}],
         [{color: []}, {background: []}],
         ["video"],
       ],
     }
+  },
+  validation: {
+
   },
   created() {
     this.getBusName()
@@ -89,6 +146,12 @@ export default {
       //  }
       //);
       document.execCommand('insertHTML', false, (html));
+    },
+    sum() {
+      this.numadj++
+    },
+    less() {
+      this.numadj--
     }
   }
 }
