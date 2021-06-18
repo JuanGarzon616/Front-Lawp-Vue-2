c82fe8f3c76fddb905f90fdec77dea5921a89c62
JuanGarzon616 <53374654+JuanGarzon616@users.noreply.github.com>
2021-06-17 20:26:21 -0500

    ertwertwert

--- src/views/layouts/users/pqruser.vue
+++ src/views/layouts/users/pqruser.vue
@@ -7,17 +7,46 @@
       <div class="pl-0 pt-36 md:pl-24 min-h-screen bg-gray-200 flex flex-col">
         <h1 class="text-3xl z-0 lg:z-10"><b>Bienvenido</b></h1>
         <p class="text-xl z-0 lg:z-10">Aqui puedes ver tu pqr mas detallada, respuestas y adjuntos.</p>
-        <div class="mt-10 p-10 w-full md:w-11/12 bg-white rounded-lg z-0 lg:z-10">
+        <div class="mt-10 mb-32 p-10 w-full md:w-11/12 bg-white rounded-lg z-0 lg:z-10 font-serif">
           <h1 class="text-3xl">Tu PQR</h1>
-          <h1 class="text-lg z-0 pt-10 font-serif">Asunto: {{ $store.getters["d/pqrsUser"][$route.params.id]['affair'] }}</h1>
-          <h1 class="text-lg z-0 font-serif" >Para: {{
-              $store.getters["d/pqrsUser"][$route.params.id].business.bussiness_name
+          <h1 class="text-lg z-0 pt-10">Asunto: {{
+              $store.getters["d/pqrsUser"][$route.params.id]['affair']
             }}</h1>
-          <h1 v-if="$store.getters['d/pqrsUser'][$route.params.id].fk_pqr_type_id===1" class="text-lg font-serif">pregunta</h1>
-          <h1 v-if="$store.getters['d/pqrsUser'][$route.params.id].fk_pqr_type_id===2" class="text-lg font-serif">queja</h1>
-          <h1 v-if="$store.getters['d/pqrsUser'][$route.params.id].fk_pqr_type_id===3" class="text-lg font-serif">reclamo</h1>
-          <VueEditor class="pt-10 pb-32 z-0" disabled
+          <h1 class="text-lg z-0 ">Para: <b>{{
+              $store.getters["d/pqrsUser"][$route.params.id].business.bussiness_name
+            }}, {{
+              $store.getters["d/pqrsUser"][$route.params.id].business.mail
+            }}</b></h1>
+          <h1 v-if="$store.getters['d/pqrsUser'][$route.params.id].fk_pqr_type_id===1" class="text-lg font-serif">
+            pregunta</h1>
+          <h1 v-if="$store.getters['d/pqrsUser'][$route.params.id].fk_pqr_type_id===2" class="text-lg font-serif">
+            queja</h1>
+          <h1 v-if="$store.getters['d/pqrsUser'][$route.params.id].fk_pqr_type_id===3" class="text-lg font-serif">
+            reclamo</h1>
+          <VueEditor class="pt-10 pb-10 z-0" disabled
                      v-model="$store.getters['d/pqrsUser'][$route.params.id].description"></VueEditor>
+          <div class=" font-serif">
+            <h1 class="text-3xl pb-5">Links</h1>
+            <p  v-for="(item, index) in $store.getters['d/pqrsUser'][$route.params.id].attachments" :key="index">
+              <a v-if="item.url" class="text-purple-500 hover:text-purple-700 text-base" v-bind:href="item.url"
+                 download="prueba">Url {{ item.url }}</a>
+            </p>
+          </div>
+          <div class=" font-serif">
+            <h1 class="text-3xl pt-10">Archivos</h1>
+            <p v-for="(item, index) in $store.getters['d/pqrsUser'][$route.params.id].attachments" :key="index">
+              <a class="text-purple-500 hover:text-purple-700" v-bind:href="item.archive" download="prueba">Adjunto
+                {{ index + 1 }}</a>
+            </p>
+          </div>
+          <h1 class="text-3xl pt-10 font-serif">Respuestas</h1>
+          <div class="pl-10 font-serif" v-for="(item, index) in $store.getters['d/pqrsUser'][$route.params.id].responses" :key="index">
+            <VueEditor class="pt-10" disabled v-if="item.response_pqr" v-model="item.response_pqr" ></VueEditor>
+            <h1 class="text-3xl pb-5">Links</h1>
+            <h1 class="text-3xl pb-5">Archivos</h1>
+          </div>
+
+
         </div>
       </div>
     </div>
@@ -53,18 +82,29 @@ export default {
   methods: {
     ...mapActions('d', ['getPqrUser']),
     onFail() {
-      // if(!this.$store.getters['d/pqrsUser'][this.$route.params.id].affair){
-      //   this.$router.push('/userlogin')
-      console.log('hola')
-      // }
     }
   },
   data() {
-    return {}
+    return {
+      customToolbar: [
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+      ],
+    }
   }
 }
 </script>
 
 <style scoped>
+
+textarea{
+  resize: none;
+}
 
 </style>
